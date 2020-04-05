1b5258214ea0e97503bc90a07b5d6512a2fb183d
kyawthura-gg <ggaryal@gmail.com>
2020-04-05 22:03:25 +0630

    adding api for world record

--- js/main.js
+++ js/main.js
@@ -11,4 +11,14 @@ window.onload = function() {
     deaths.start();
     var recovered = new CountUp("recovered", 1);
     recovered.start();
+    const apiUrl = "https://covidapi.info/api/v1/global";
+    // const urlParams = new URLSearchParams(apiUrl);
+    // // const product = urlParams.getAll('result');
+    // console.log(urlParams);
+    var confirm = new CountUp("golobal-confirmed", 1197405, options);
+    confirm.start();
+    var deaths = new CountUp("golobal-deaths", 64606);
+    deaths.start();
+    var recovered = new CountUp("golobal-recovered", 246152);
+    recovered.start();
 };
