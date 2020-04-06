8f1e8c4779a5394fd1d1ad9f6470e231cfa0a2e0
kyawthura-gg <ggaryal@gmail.com>
2020-04-06 21:24:40 +0630

    fixed global api and menu

--- js/app.js
+++ js/app.js
@@ -66,12 +66,40 @@ $(document).ready(function() {
         }
     };
     // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function($el) {
+            $el.addEventListener('click', function() {
+                // Get the target from the "data-target" attribute
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    }
+
+    function mobile_expandable_menu() {
+        if ($(window).width() < 768) {
+            $('.navbar-link').next('.navbar-dropdown').hide();
+            $('.navbar-link').on('click', function() {
+                $(this).next('.navbar-dropdown').slideToggle();
+            });
+        } else {
+            $('.navbar-link').next('.navbar-dropdown').css('display', '');
+            $('.navbar-link').unbind('click');
+        }
+    }
+    var screen_resize_timout;
+    $(window).on("resize", function(e) {
+        clearTimeout(screen_resize_timout);
+        screen_resize_timout = setTimeout(mobile_expandable_menu, 500);
     });
-    // change white icon if it's dark theme
+    mobile_expandable_menu();
+    // change white icon if browser is dark theme
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
         link.type = 'image/x-icon';
--- js/main.js
+++ js/main.js
@@ -5,20 +5,26 @@ const options = {
     duration: 5
 };
 window.onload = function() {
-    var confirm = new CountUp("confirmed", 21, options);
+    var confirm = new CountUp("confirmed", 22, options);
     confirm.start();
     var deaths = new CountUp("deaths", 1);
     deaths.start();
-    var recovered = new CountUp("recovered", 1);
+    var recovered = new CountUp("recovered", 0);
     recovered.start();
     const apiUrl = "https://covidapi.info/api/v1/global";
-    // const urlParams = new URLSearchParams(apiUrl);
-    // // const product = urlParams.getAll('result');
-    // console.log(urlParams);
-    var confirm = new CountUp("golobal-confirmed", 1197405, options);
-    confirm.start();
-    var deaths = new CountUp("golobal-deaths", 64606);
-    deaths.start();
-    var recovered = new CountUp("golobal-recovered", 246152);
-    recovered.start();
+    var gConfirmed = 0;
+    var gDeaths = 0;
+    var gRecovered = 0;
+    fetch(apiUrl).then(res => res.json()).then(data => data.result).then(function(covidData) {
+        gConfirmed = covidData.confirmed;
+        gDeaths = covidData.deaths;
+        gRecovered = covidData.recovered;
+        console.log(gConfirmed);
+        var confirm = new CountUp("golobal-confirmed", gConfirmed, options);
+        confirm.start();
+        var deaths = new CountUp("golobal-deaths", gDeaths);
+        deaths.start();
+        var recovered = new CountUp("golobal-recovered", gRecovered);
+        recovered.start();
+    })
 };
