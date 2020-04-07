cfb1aea0224051e7ec6043ea2740143e7c1b105a
kyawthura-gg <ggaryal@gmail.com>
2020-04-07 21:54:28 +0630

    moved to local

---
+++ system/resources/views/contactus.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="center-div">
+        <h1 class="coming-s">Coming Soon</h1>
+        <h1 class="coming-s">Please send email to (inquiry@covid19burma.org)</h1>
+    </div>
+</div>
+@endsection
