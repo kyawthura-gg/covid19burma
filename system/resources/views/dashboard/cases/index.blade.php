5578d218be4dd6278f9defed25d0770c3e86e8ec
kyawthura-gg <ggaryal@gmail.com>
2020-04-13 22:19:33 +0630

    added case count in view

--- system/resources/views/dashboard/cases/index.blade.php
+++ system/resources/views/dashboard/cases/index.blade.php
@@ -19,42 +19,59 @@
         <p>{{ $message }}</p>
     </div>
     @endif
+    <div class="columns">
+        <div class="column is-8">
+            <table class="table table-bordered">
+                <tr>
+                    <th>No</th>
+                    <th>State</th>
+                    <th>City</th>
+                    <th>Date</th>
+                    <th>Cases</th>
+                    <th>Deaths</th>
+                    <th>Recovered</th>
+                    <th width="280px">Action</th>
+                </tr>
+                @foreach ($cases as $case)
+                <tr>
+                    <td>{{ ++$i }}</td>
+                    <td>{{ $case->state }}</td>
+                    <td>{{ $case->city }}</td>
+                    <td>{{ $case->date_confirm }}</td>
+                    <td>{{ $case->confirm_case  }}</td>
+                    <td>{{ $case->deaths }}</td>
+                    <td>{{ $case->recovered }}</td>
+                    <td>
+                        <form action="{{ route('cases.destroy',$case->id) }}" method="POST">
 
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>State</th>
-            <th>City</th>
-            <th>Date</th>
-            <th>Cases</th>
-            <th>Deaths</th>
-            <th>Recovered</th>
-            <th width="280px">Action</th>
-        </tr>
-        @foreach ($cases as $case)
-        <tr>
-            <td>{{ ++$i }}</td>
-            <td>{{ $case->state }}</td>
-            <td>{{ $case->city }}</td>
-            <td>{{ $case->date_confirm }}</td>
-            <td>{{ $case->confirm_case  }}</td>
-            <td>{{ $case->deaths }}</td>
-            <td>{{ $case->recovered }}</td>
-            <td>
-                <form action="{{ route('cases.destroy',$case->id) }}" method="POST">
+                            <a class="button is-primary is-small" href="{{ route('cases.edit',$case->id) }}"><i class="far fa-edit fa-lg"></i></a>
 
-                    <a class="button is-primary is-small" href="{{ route('cases.edit',$case->id) }}"><i class="far fa-edit fa-lg"></i></a>
+                            @csrf
+                            @method('DELETE')
 
-                    @csrf
-                    @method('DELETE')
-
-                    <button type="submit" class="button is-danger is-small" onclick="return confirm('Are you sure want to delete?')"><i class="fas fa-trash fa-lg"></i></button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-    {!! $cases->links() !!}
+                            <button type="submit" class="button is-danger is-small" onclick="return confirm('Are you sure want to delete?')"><i class="fas fa-trash fa-lg"></i></button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+            {!! $cases->links() !!}
+        </div>
+        <div class="column is-4 ">
+            <div class="level-item">
+                <div class="title has-text-grey">Confrim: {{$total_confirm}}</div>
+            </div>
+            <div class="level-item">
+                <div class="title has-text-danger">Deaths: {{$deaths}}</div>
+            </div>
+            <div class="level-item">
+                <div class="title has-text-primary">Recovered: {{$recovered}}</div>
+            </div>
+            <div class="level-item">
+                <div class="title has-text-success">Active: {{$active}}</div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
