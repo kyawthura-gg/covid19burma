63fb703defefc678e8443b0a9356f645fbeae50f
kyawthura-gg <ggaryal@gmail.com>
2020-04-18 21:42:01 +0630

    added view for case details

---
+++ system/resources/views/case.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="columns">
+        <div class="column is-3">
+            <div class="card">
+                <div class="card-content">
+                    <p class="title">
+                        Case 21
+                    </p>
+                    <p class="subtitle">
+                        Jeff Atwood
+                    </p>
+                </div>
+                <footer class="card-footer">
+                    <p class="card-footer-item">
+                        <span>
+                            View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                        </span>
+                    </p>
+                </footer>
+            </div>
+        </div>
+        <div class="column is-3">
+            <div class="card">
+                <div class="card-content">
+                    <p class="title">
+                        Case 21
+                    </p>
+                    <p class="subtitle">
+                        Jeff Atwood
+                    </p>
+                </div>
+                <footer class="card-footer">
+                    <p class="card-footer-item">
+                        <span>
+                            View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                        </span>
+                    </p>
+                </footer>
+            </div>
+        </div>
+        <div class="column is-3">
+            <div class="card">
+                <div class="card-content">
+                    <p class="title">
+                        Case 21
+                    </p>
+                    <p class="subtitle">
+                        Jeff Atwood
+                    </p>
+                </div>
+                <footer class="card-footer">
+                    <p class="card-footer-item">
+                        <span>
+                            View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                        </span>
+                    </p>
+                </footer>
+            </div>
+        </div>
+        <div class="column is-3">
+            <div class="card">
+                <div class="card-content">
+                    <p class="title">
+                        Case 21
+                    </p>
+                    <p class="subtitle">
+                        Jeff Atwood
+                    </p>
+                </div>
+                <footer class="card-footer">
+                    <p class="card-footer-item">
+                        <span>
+                            View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                        </span>
+                    </p>
+                </footer>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
