6938aa6fb814764b2442361886f0768355dd7583
kyawthura-gg <ggaryal@gmail.com>
2020-04-26 22:19:25 +0630

    added translation

--- system/resources/views/news.blade.php
+++ system/resources/views/news.blade.php
@@ -4,29 +4,31 @@
 <div class="container">
     <!-- Developers -->
     <div class="columns is-multiline">
+        @foreach ($blogs as $blog)
         <div class="column is-4 is-mobile">
-            @foreach ($blogs as $blog)
             <div class="custom-card">
                 <div class="card-image">
                     <figure class="image">
-                        <img src="{{$blog->source_image}}" alt="Image">
+                        <img src="{{ URL::to('/') }}/uploads/news/{{ $blog->source_image}}" alt="Image" style="border-radius: 10px">
                     </figure>
                 </div>
                 <div class="custom-content">
                     <div class="level">
                         <div class="level-left">
-                            <div class="item-author is-size-4"><a href="{{$blog->source_link}}">{{$blog->source}}</a></div>
+                            <div class="item-author is-size-4">{{$blog->source}}</div>
                         </div>
                         <div class="level-right">
                             <time datetime="2018-06-07">{{$blog->source_date}}</time>
                         </div>
                     </div>
-                    <p class="mb-10">{{$blog->details}}</p>
-                    <a href="{{$blog->source_link}}">Read More</p>
+                    <p class="mb-10">{{ $blog->details }}</p>
+                    <div class="has-text-centered mt-10">
+                        <a href="{{ $blog->source_link }}">{{__('news.readmore')}}</a>
+                    </div>
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
     <!-- End Developers -->
 </div>
