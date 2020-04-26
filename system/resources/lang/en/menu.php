b36769e00f899d23c9c8e9fe2fd0f5aa996c7ced
kyawthura-gg <ggaryal@gmail.com>
2020-04-26 22:20:10 +0630

    added menu translation

---
+++ system/resources/lang/en/menu.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'cluster' => 'Cluster',
+    'news' => 'News',
+    'aboutus' => 'About Us',
+    'more' => 'More',
+    'report' => 'Report Issue',
+    'myanmar' => 'Myanmar',
+];
---
+++ system/resources/lang/mm/menu.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'home' => 'မူလ',
+    'cluster' => 'ကူးစက်မှု',
+    'news' => 'သတင်း',
+    'aboutus' => 'About Us',
+    'more' => 'နောက်ထပ်',
+    'report' => 'Report Issue',
+    'myanmar' => 'မြန်မာ',
+];
