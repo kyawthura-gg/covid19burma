36f46dcb650011fb04d9702f29ac6edd772bc3f2
kyawthura-gg <ggaryal@gmail.com>
2020-05-17 16:00:36 +0630

    created column for graphs

---
+++ system/resources/views/graph.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="content">
+    <div class="columns">
+        <div class="column is-4">
+            graph 1
+        </div>
+        <div class="column is-4">
+            graph 2
+        </div>
+        <div class="column is-4">
+            graph 4
+        </div>
+    </div>
+</div>
+@endsection
