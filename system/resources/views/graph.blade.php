828177a24db90c47accb66cbf1b78df776027653
kyawthura-gg <ggaryal@gmail.com>
2020-05-23 14:12:28 +0630

    added graph canvas

--- system/resources/views/graph.blade.php
+++ system/resources/views/graph.blade.php
@@ -4,13 +4,22 @@
 <div class="content">
     <div class="columns">
         <div class="column is-4">
-            graph 1
+            <div class="total-box">
+                <canvas id="reportDate" width="400" height="400"></canvas>
+                <progress id="animationProgressDate" max="1" value="0" style="display: none"></progress>
+            </div>
         </div>
         <div class="column is-4">
-            graph 2
+            <div class="total-box">
+                <canvas id="myChart" width="400" height="400"></canvas>
+                <progress id="animationProgress" max="1" value="0" style="display: none"></progress>
+            </div>
         </div>
         <div class="column is-4">
-            graph 4
+            <div class="total-box">
+                <canvas id="caseReport" width="400" height="400"></canvas>
+                <progress id="casaeAnimationProgress" max="1" value="0" style="display: none"></progress>
+            </div>
         </div>
     </div>
 </div>
