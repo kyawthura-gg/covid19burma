73329081b9d4d6ec51d89bfeee744a894c7a7710
kyawthura-gg <ggaryal@gmail.com>
2020-05-23 14:13:20 +0630

    added more canvas

--- system/resources/views/graph.blade.php
+++ system/resources/views/graph.blade.php
@@ -22,6 +22,26 @@
             </div>
         </div>
     </div>
+    <div class="columns">
+        <div class="column is-4">
+            <div class="total-box">
+                <canvas id="reportDate" width="400" height="400"></canvas>
+                <progress id="animationProgressDate" max="1" value="0" style="display: none"></progress>
+            </div>
+        </div>
+        <div class="column is-4">
+            <div class="total-box">
+                <canvas id="myChart" width="400" height="400"></canvas>
+                <progress id="animationProgress" max="1" value="0" style="display: none"></progress>
+            </div>
+        </div>
+        <div class="column is-4">
+            <div class="total-box">
+                <canvas id="caseReport" width="400" height="400"></canvas>
+                <progress id="casaeAnimationProgress" max="1" value="0" style="display: none"></progress>
+            </div>
+        </div>
+    </div>
 </div>
 <script>
     var urlState = "{{url('reportByState')}}";
