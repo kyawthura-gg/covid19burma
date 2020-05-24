9692e16154b64f9df8c4dc7d668a433a54b60a1a
kyawthura-gg <ggaryal@gmail.com>
2020-05-24 16:07:33 +0630

    added assets for active report

--- system/resources/views/graph.blade.php
+++ system/resources/views/graph.blade.php
@@ -37,8 +37,8 @@
         </div>
         <div class="column is-4">
             <div class="total-box">
-                <canvas id="caseReport" width="400" height="400"></canvas>
-                <progress id="casaeAnimationProgress" max="1" value="0" style="display: none"></progress>
+                <canvas id="activeReport" width="400" height="400"></canvas>
+                <progress id="activeAnimationProgress" max="1" value="0" style="display: none"></progress>
             </div>
         </div>
     </div>
@@ -53,9 +53,10 @@
     var recovered = "{{trans('home.recovered')}}";
     var daily = "{{trans('home.daily')}}";
 </script>
-<script type="text/javascript" src="{{ asset('js/chartjs/caseReport.js?v=c298cs6saafa8233d')}}"></script>
-<script type="text/javascript" src="{{ asset('js/chartjs/deathReport.js?v=c298cs6saafa8233d')}}"></script>
+<script type="text/javascript" src="{{ asset('js/chartjs/caseReport.js?v=c298cs6saaafa8233d')}}"></script>
+<script type="text/javascript" src="{{ asset('js/chartjs/deathReport.js?v=c298cs6saaafa8233d')}}"></script>
 <script type="text/javascript" src="{{ asset('js/chartjs/recoverReport.js?v=c298acs6saafa8233d')}}"></script>
 <script type="text/javascript" src="{{ asset('js/chartjs/reportByDate.js?v=c298cs6saafa8233d')}}"></script>
-<script type="text/javascript" src="{{ asset('js/chartjs/reportByState.js?v=c298csas6safa8233d')}}"></script>
+<script type="text/javascript" src="{{ asset('js/chartjs/reportByState.js?v=c298csasas6safa8233d')}}"></script>
+<script type="text/javascript" src="{{ asset('js/chartjs/activeReport.js?v=c298acs6saafa8233d')}}"></script>
 @endsection
